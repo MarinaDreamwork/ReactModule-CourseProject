c68e322b6b41a5440572fdc300536727ac2d1c2e
Marina Trushina <silaevamo@gmail.com>
2022-03-22 10:15:35 +0300

    First practice task

---
+++ src/components/users.jsx
@@ -0,0 +1,72 @@
+import { useState } from 'react';
+import api from '../api';
+
+const Users = () => {
+  const [users, setUsers] = useState(api.users.fetchAll());
+  const [count, setCount] = useState(api.users.fetchAll().length);
+
+  const handleDelete = (userId) => {
+    setUsers(prevState => prevState.filter(td => td._id !== userId));
+    setCount(prevState => prevState - 1);
+  } 
+  const renderPhrase = (number) => {
+    let basePhrase = ' с тобой сегодня';
+    let rightPhrase = '';
+  
+    if(number === 2 || number === 3 || number === 4) {
+      rightPhrase = 'человека тусанут';
+    } else if(number === 0) {
+      rightPhrase = 'Никто не тусанёт';
+    } else {
+      rightPhrase = 'человек тусанёт';
+    }
+    return rightPhrase + basePhrase;
+  }
+
+  const getMessageClasses = () => {
+    let classes = 'badge m-2 ';
+    classes += count === 0 ? 'bg-danger' : 'bg-primary';
+    return classes;
+  }
+
+    return (
+      <>
+        <h2>
+          <span className={getMessageClasses()}>
+          {count === 0 ? '' : count} {renderPhrase(count)}
+          </span>
+        </h2>
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">Имя</th>
+              <th scope="col">Качества</th>
+              <th scope="col">Профессия</th>
+              <th scope="col">Встретился, раз</th>
+              <th scope="col">Оценка</th>
+              <th scope="col"></th>
+            </tr>
+          </thead> 
+          <tbody>
+            {
+              users.map(user => {
+                return (
+                  <tr key={user._id}>
+                    <td>{user.name}</td>
+                    <td>{user.qualities.map((quality => <span key={quality._id} className={'m-2 badge bg-' + quality.color}>{quality.name}</span> ))}</td>
+                    <td>{user.profession.name}</td>
+                    <td>{user.completedMeetings}</td>
+                    <td>{user.rate} /5</td>
+                    <td><button className='badge bg-danger' onClick={() => handleDelete(user._id)}>delete</button></td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
+      </>
+    )
+}
+
+
+export default Users;
