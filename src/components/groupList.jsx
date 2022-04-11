007d92fc39433802d9b4802ed12c4201dce57791
Marina Trushina <silaevamo@gmail.com>
2022-04-11 18:00:53 +0300

    Lesson 5 - added filtration functionality

---
+++ src/components/groupList.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+
+const GroupList = ({ items, valueProperty, contentProperty, onItemSelect, currentItem }) => {
+  return (
+    <ul className='list-group'>
+      {Object.keys(items).map(item => {
+        return <li key={items[item][valueProperty]} className={'list-group-item' + (items[item] === currentItem ? ' active' : '')} onClick={() => onItemSelect(items[item])} role='button'>{items[item][contentProperty]}</li>;
+      })}
+    </ul>
+  );
+};
+GroupList.defaultProps = {
+  valueProperty: '_id',
+  contentProperty: 'name'
+};
+
+GroupList.propTypes = {
+  items: PropTypes.object.isRequired,
+  valueProperty: PropTypes.string.isRequired,
+  contentProperty: PropTypes.string.isRequired,
+  onItemSelect: PropTypes.func.isRequired,
+  currentItem: PropTypes.object
+};
+
+export default GroupList;
--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,64 +1,91 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { paginate } from '../utils/paginate';
+import api from '../api';
 import Pagination from './pagination';
 import SearchStatus from './searchStatus';
 import User from './user';
 import PropTypes from 'prop-types';
+import GroupList from './groupList';
 
-const Users = ({ users, onDelete, onToggleBookMark, quantityOfUsers }) => {
+const Users = ({ users, onDelete, onToggleBookMark }) => {
   const pageSize = 4;
 
   const [currentPage, setCurrentPage] = useState(1);
+  const [professions, setProfessions] = useState();
+  const [selectedProf, setSelectedProf] = useState();
 
   const handleChangeClick = (pageIndex) => {
     setCurrentPage(pageIndex);
   };
 
-  const partOfUsers = paginate(users, pageSize, currentPage);
+  const handleProfessionSelect = (item) => {
+    setSelectedProf(item);
+  };
+
+  const clearFilter = () => setSelectedProf();
+  const filteredUsers = selectedProf
+    ? users.filter((user) => {
+      return user.profession === selectedProf;
+    })
+    : users;
+  const count = filteredUsers.length;
+  const partOfUsers = paginate(filteredUsers, pageSize, currentPage);
+
+  useEffect(() => {
+    api.professions.fetchAll().then(data => setProfessions(data));
+  }, []);
 
   return (
-    <>
-      {
-        <SearchStatus length={quantityOfUsers} />
-      }
-      <table className='table table-hover'>
-        <thead>
-          <tr>
-            <th scope='col'>Имя</th>
-            <th scope='col'>Качества</th>
-            <th scope='col'>Профессия</th>
-            <th scope='col'>Встретился, раз</th>
-            <th scope='col'>Оценка</th>
-            <th scope='col'>Избранное</th>
-            <th scope='col'></th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            partOfUsers.map(user => (
-              <User
-                key={user._id}
-                user={user}
-                onDelete={onDelete}
-                onToggleBookMark={onToggleBookMark} />
-            ))
-          }
-        </tbody>
-      </table>
-      <Pagination
-        usersCount={quantityOfUsers}
-        pageSize={pageSize}
-        currentPage={currentPage}
-        onChangeClick={handleChangeClick}/>
-    </>
+    <div className='d-flex'>
+      {professions && (
+        <div className='d-flex flex-column flex-shrink-0 p-3 pt-4'>
+          <GroupList items={professions} onItemSelect={handleProfessionSelect} currentItem={selectedProf}/>
+          <button className='btn btn-secondary mt-2' onClick={clearFilter}>Очистить</button>
+        </div>)}
+      <div className='d-flex flex-column'>
+        <div className='d-flex justify-content-center'>
+          <SearchStatus length={count} />
+        </div>
+        <table className='table table-hover'>
+          <thead>
+            <tr>
+              <th scope='col'>Имя</th>
+              <th scope='col'>Качества</th>
+              <th scope='col'>Профессия</th>
+              <th scope='col'>Встретился, раз</th>
+              <th scope='col'>Оценка</th>
+              <th scope='col'>Избранное</th>
+              <th scope='col'></th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              partOfUsers.map(user => (
+                <User
+                  key={user._id}
+                  user={user}
+                  onDelete={onDelete}
+                  onToggleBookMark={onToggleBookMark} />
+              ))
+            }
+          </tbody>
+        </table>
+        <div className='d-flex justify-content-center'>
+          <Pagination
+            usersCount={count}
+            pageSize={pageSize}
+            currentPage={currentPage}
+            onChangeClick={handleChangeClick}/>
+        </div>
+      </div>
+    </div>
   );
 };
 
 Users.propTypes = {
   users: PropTypes.array.isRequired,
   onDelete: PropTypes.func.isRequired,
-  onToggleBookMark: PropTypes.func.isRequired,
-  quantityOfUsers: PropTypes.number.isRequired
+  onToggleBookMark: PropTypes.func.isRequired
 };
 
 export default Users;
