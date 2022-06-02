9fda632ca032a173e647d35167d9e9249976e2a9
Marina Trushina <silaevamo@gmail.com>
2022-06-02 13:35:13 +0300

    Lesson #15 done

--- src/layout/main.jsx
+++ src/layout/main.jsx
@@ -1,6 +1,20 @@
+import { useMockData } from '../utils/mockData';
+
 const Main = () => {
+  const { initialize, status, progress, error } = useMockData();
+  const handleClick = () => {
+    initialize();
+  };
   return (
-    <h2>Main</h2>
+    <div className='m-3'>
+      <h2>Инициализация данных Firebase</h2>
+      <ul>
+        <li>Status: {status}</li>
+        <li>Progress: {progress}</li>
+        { error && <li>Error: {error}</li>}
+      </ul>
+      <button className='btn btn-primary mt-3' onClick={handleClick}>Initialize data</button>
+    </div>
   );
 };
 
--- src/services/http.service.js
+++ src/services/http.service.js
@@ -1,11 +1,37 @@
 import axios from 'axios';
 // import logger from './log.service';
 import { toast } from 'react-toastify';
-import config from '../config.json';
+import configFile from '../config.json';
 
-axios.defaults.baseURL = config.apiEndpoint;
+axios.defaults.baseURL = configFile.apiEndpoint;
 
-axios.interceptors.response.use((res) => res,
+axios.interceptors.request.use(
+    function (config) {
+        if (configFile.isFirebase) {
+            const containSlash = /\/$/gi.test(config.url);
+            config.url = (containSlash ? config.url.slice(0, -1) : config.url) + '.json';
+            return config;
+        }
+    }, function (error) {
+        return Promise.reject(error);
+    }
+);
+
+function transformData(data) {
+    return data ? Object.keys(data).map(key => ({
+        ...data[key]
+    })) : [];
+}
+
+axios.interceptors.response.use(
+    (res) => {
+         if (configFile.isFirebase) {
+            res.data = {
+                content: transformData(res.data)
+            };
+            return res;
+        };
+    },
     function (error) {
         const expectedErrors = error.response && error.response.status >= 400 && error.response.status < 500;
             if (!expectedErrors) {
